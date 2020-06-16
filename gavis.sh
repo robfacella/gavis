f77a1646f70633d9f38f60b22a3e3e2db20c8fcc
Rob Facella <rob.facella@gmail.com>
2020-06-16 15:03:53 -0400

    Disk Use Calculator function

--- gavis.sh
+++ gavis.sh
@@ -2,6 +2,20 @@
 #Set IFS to new line.
 #IFS=$'\n'
 
+CalcDU () {
+   startSize=0
+   for file in $( find "$folder" -name '*.*' ); do
+      	#Calculate starting Size of Files.
+        #echo "$file"
+	val=$( du "$file" | awk '{print $1}' ) 
+	#du|awk statement gets just size of files; stores in val
+	startSize=`expr $startSize + $val`
+   done
+   echo "Starting size of File(s) within $folder: $startSize k "
+   ##This outputs 72 k total file size for the files within the testFiles directory
+   ##HOWEVER, right clicking on testFiles and observing the directory's properties shows only 46.5 kB of disk usage.
+}
+
 echo "Iterates over image files with xviewer"
 echo "In a directory and all of its subdirectories."
 echo ""
@@ -9,17 +23,7 @@ echo "Enter a path(--h for more info): "
 folder="testFiles"
 #read folder
 
-startSize=0
-for file in $( find "$folder" -name '*.*' ); do
-	#Calculate starting Size of Files.
-        #echo "$file"
-	val=$( du "$file" | awk '{print $1}' ) 
-	#du|awk statement gets just size of files; stores in val
-	startSize=`expr $startSize + $val`
-done
-echo "Starting size of File(s) within $folder: $startSize k "
-##This outputs 72 k total file size for the files within the testFiles directory
-##HOWEVER, right clicking on testFiles and observing the directory's properties shows only 46.5 kB of disk usage.
+CalcDU
 
 ##Opens file location in a new window with the XVIEWER program.
 ##-w makes xviewer use a single window, will replace an old instance of itself INSTEAD of just opening a new instance. (Should help reduce acidental/negligent RAM leaks.)
