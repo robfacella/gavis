f28a39fd087e8a865ef2d052dc71fef51881706e
Rob Facella <rob.facella@gmail.com>
2021-10-19 17:09:00 -0400

    Runtime

--- pngDimensionalPurge.sh
+++ pngDimensionalPurge.sh
@@ -4,6 +4,7 @@ minH=200
 minW=200
 toPurge=0
 toSave=0
+start=`date +%s`
 # use imagemagick to get png dimensions
 #32x32
 file="/media/pi/38B62D40724FA264/MintPng_Fri_Oct_15_23_41_32_2021/png/461768368.png"
@@ -11,12 +12,13 @@ file="/media/pi/38B62D40724FA264/MintPng_Fri_Oct_15_23_41_32_2021/png/461768368.
 #file="/media/pi/38B62D40724FA264/MintPng_Fri_Oct_15_23_41_32_2021/png/461774106.png"
 folder="/media/pi/38B62D40724FA264/MintPng_Fri_Oct_15_23_41_32_2021/png/"
 DimensionCheck () {
-	width=$(identify -format "%w" "$file")> /dev/null
-	height=$(identify -format "%h" "$file")> /dev/null
+	width=$(identify -format "%w" "$image")> /dev/null
+	height=$(identify -format "%h" "$image")> /dev/null
 	#echo "Width: $width ; Height: $height"
 	if [ $width -lt $minW ] || [ $height -lt $minH ]
 	then
 		#echo "at least one of the dimension is below minimum threshold"
+		
 		let toPurge++
 	else
 		#echo "Width: $width ; Height: $height"
@@ -27,8 +29,11 @@ FileInPath () {
 	for image in $( find "$folder" -name "*.png"); do
 		DimensionCheck
 	done
-	echo "To Purge: $toPurge"
-	echo "To Save: $toSave"
+	echo "Purged: $toPurge"
+	echo " Saved: $toSave"
 }
 
 FileInPath
+end=`date +%s`
+runtime=$((end-start))
+echo "Total Runtime in seconds: $runtime"
