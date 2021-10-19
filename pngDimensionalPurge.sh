3ff9c4b5f3e8e7b5996816ff0a0d202e0e217c15
Rob Facella <rob.facella@gmail.com>
2021-10-19 14:49:44 -0400

    Logic Check

--- pngDimensionalPurge.sh
+++ pngDimensionalPurge.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+minH=200
+minW=200
 # use imagemagick to get png dimensions
 file="/media/pi/38B62D40724FA264/MintPng_Fri_Oct_15_23_41_32_2021/png/461768368.png"
 width=$(identify -format "%w" "$file")> /dev/null
 height=$(identify -format "%h" "$file")> /dev/null
-echo "Width: $width ; Height: $height"
+#echo "Width: $width ; Height: $height"
+if [ $width -lt $minW ] || [ $height -lt $minH ]
+then
+	echo "at least one of the dimension is below minimum threshold"
+fi
