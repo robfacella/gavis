44db49ee2e056bcf7d56d56da29c68abdbc68f18
robfacella <rob.facella@gmail.com>
2025-09-20 21:38:01 -0400

    arg input

--- Scripts/ShredDir.sh
+++ Scripts/ShredDir.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-infile="$HOME/Downloads/testFiles"
+# Worked with:
+# bash Scripts/ShredDir.sh $HOME/Downloads/testFiles/
+# which is 'Good-Enough' for current use case
+
+# Uses first command line argument (after the script name)
+# As the 'Input String for the Directory to Shred'
+infile="$1"
 #ls $infile
 
-# From the deepest point in the directory,
-# Shred by Zero'ing, Single-Pass, Unlinking Files Shreded
+# From the 'deepest point' in the directory tree,
+# 'Shred' by '[z]ero-ing', 'Si[n]gle-Pass', '[u]nlinking Files' Shreded with '[v]erbose' output
 find $infile -depth -type f -exec shred -fvz -n0 -u {} \;
 
-# Afterwards the directories *should* be empty and thus removable
+# Afterwards the [d]irectories *should* be empty and thus removable
 find $infile -depth -type d -exec rm -dv {} \;
 
 ## Remove Root of Traversed Directories last
